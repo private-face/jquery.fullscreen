1e8b56dc03d4f0d2da60533c71af2ada4d79404f
Vladimir Zhuravlev <private.face@gmail.com>
2013-01-18 23:53:10 +0700

    keydown event delegation now supports jq1.7

--- dev/fallback-fullscreen.js
+++ dev/fallback-fullscreen.js
@@ -3,6 +3,7 @@ var FullScreenFallback = function() {
 };
 
 extend(FullScreenFallback, FullScreenAbstract, {
+	__JQ_LT_17: parseFloat($.fn.jquery) < 1.7,
 	__isFullScreen: false,
 	__allowedKeys: [
 		// left arrow, right arrow, up arrow, down arrow, space, page up, page down, home, end, tab,
@@ -10,6 +11,18 @@ extend(FullScreenFallback, FullScreenAbstract, {
 		// shift, control, alt, cmd, win
 		16, 17, 18, 224, 91
 	],
+	__delegateKeydownHandler: function() {
+		var $doc = $(document);
+		$doc.delegate('*', 'keydown.fullscreen', $.proxy(this.__keydownHandler, this));
+		var data = JQ_LT_17 ? $doc.data('events') : $._data(document).events;
+		var events = data['keydown'];
+
+		if (!JQ_LT_17) {
+			events.splice(0, 0, events.splice(events.delegateCount - 1, 1)[0]);
+		} else {
+			data.live.unshift(data.live.pop());
+		}
+	},
 	__keydownHandler: function(e) {
 		if (!this.isFullScreen() || $.inArray(e.which, this.__allowedKeys) !== -1) {
 			return true;
@@ -20,8 +33,8 @@ extend(FullScreenFallback, FullScreenAbstract, {
 	},
 	_init: function() {
 		FullScreenFallback._super._init.apply(this, arguments);
-		
-		$(document).delegate('*', 'keydown.fullscreen', $.proxy(this.__keydownHandler, this)); // use delegateFirst?
+		this.__delegateKeydownHandler();
+
 	},
 	open: function(elem) {
 		FullScreenFallback._super.open.apply(this, arguments);
