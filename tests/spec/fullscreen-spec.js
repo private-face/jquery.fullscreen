3983a76c7cac0b0de89ddc427ac4e7d23ea445ac
Vladimir Zhuravlev <private.face@gmail.com>
2013-01-30 23:44:22 +0700

    tests

---
+++ tests/spec/fullscreen-spec.js
@@ -0,0 +1,103 @@
+$(function() {
+
+	describe('jquery.fullscreen', function() {
+		var fscreenState, fscreenElement,
+			fscreenOpen, fscreenClose;
+
+		$(document).bind('fscreenchange', function(e, fs, fe) {
+			fscreenState = fs;
+			fscreenElement = fe;
+		});
+
+		$('body').bind('fscreenopen', function() {
+			fscreenOpen = true;
+		});
+
+		$('body').bind('fscreenclose', function() {
+			fscreenClose = true;
+		});
+
+		$('body').bind('click.open', function(e) {
+			$(this).fullscreen({
+				toggleClass: "fscreen"
+			});
+			$('body').unbind('.open');
+			e.stopImmediatePropagation();
+		});
+
+		$('body').bind('click.close', function(e) {
+			$.fullscreen.exit();
+			$('body').unbind('.close');
+			return false;
+		});
+
+		// check browser is not in fs mode
+		it('should not be in fullscreen mode', function() {
+			expect($.fullscreen.isFullScreen()).toBeFalsy();
+		});
+
+		// checking fs mode
+		describe('go fullscreen', function() {
+
+			// check event "fscreenchange" fired on document
+			it('event "fscreenchange" should be fired on document', function() {
+				$('.specSummary').last().append('<h4>click on the page to continue testing...</h4>');
+				waitsFor(function() {
+					return fscreenState === true;
+				}, '"fscreenchange" to be fired', 60000);
+			});
+
+			// check event "fscreenopen" fired on element
+			it('event "fscreenopen" should be fired on element', function() {
+				$('.specSummary h4').remove();
+				expect(fscreenOpen).toBeTruthy();
+			});
+
+			// check browser is in fs mode
+			it('should be in fullscreen mode', function() {
+				expect($.fullscreen.isFullScreen()).toBeTruthy();
+			});
+
+			// check fs element
+			it('body should be current fullscreen element', function() {
+				expect(fscreenElement).toBe(document.body);
+			});
+
+			// check fs element has class 'fscreen'
+			it('body should have "fscreen" class', function() {
+				expect($('body').hasClass('fscreen')).toBeTruthy();
+			});
+		});
+
+
+		// checking exit fs
+		describe('exit fullscreen', function() {
+			// check event "fscreenchange" fired on document
+			it('event "fscreenchange" should be fired on document', function() {
+				$('.specSummary').last().append('<h4>click on the page again to continue testing...</h4>');
+				waitsFor(function() {
+					return fscreenState === false;
+				}, '"fscreenchange" to be fired', 60000);
+			});
+
+			// check event "fscreenclose" fired on element
+			it('event "fscreenclose" should be fired on element', function() {
+				$('.specSummary h4').remove();
+				expect(fscreenClose).toBeTruthy();
+			});
+
+			// check browser is not in fs mode
+			it('should not be in fullscreen mode', function() {
+				expect($.fullscreen.isFullScreen()).toBeFalsy();
+			});
+
+			// check exfs element has no class 'fscreen'
+			it('body should not have "fscreen" class', function() {
+				expect($('body').hasClass('fscreen')).toBeFalsy();
+			});
+		});
+
+
+	});
+
+});
---
+++ tests/spec/utils-spec.js
@@ -0,0 +1,66 @@
+describe('Utils', function() {
+
+	describe('native()', function() {
+		// spec and opera
+		var specObjElem = {
+				requestFullscreen: function() {}
+			},
+			specObjDoc = {
+				exitFullscreen: function() {},
+				fullscreenEnabled: false,
+				fullscreenElement: null
+			};
+
+		it('should detect unprefixed FS API properties and methods', function() {
+			expect(native(specObjElem, 'requestFullscreen')).toBeDefined();
+			expect(native(specObjDoc, 'exitFullscreen')).toBeDefined();
+			expect(native(specObjDoc, 'fullscreenEnabled')).toBeDefined();
+			expect(native(specObjDoc, 'fullscreenElement')).toBeDefined();
+		});
+
+		// webkit
+		var webkitObjElem = {
+				webkitRequestFullscreen: function() {}
+			},
+			webkitObjDoc = {
+				webkitExitFullscreen: function() {},
+				webkitFullscreenEnabled: false,
+				webkitFullscreenElement: null
+			};
+
+		it('should detect -webkit-prefixed FS API properties and methods', function() {
+			expect(native(webkitObjElem, 'requestFullscreen')).toBeDefined();
+			expect(native(webkitObjDoc, 'exitFullscreen')).toBeDefined();
+			expect(native(webkitObjDoc, 'fullscreenEnabled')).toBeDefined();
+			expect(native(webkitObjDoc, 'fullscreenElement')).toBeDefined();
+		});
+
+		// old webkit
+		var oldWebkitcObjDoc = {
+				webkitCancelFullscreen: function() {}
+			};
+
+		it('should detect old-webkit "webkitCancelFullscreen" methods as "exitFullscreen"', function() {
+			expect(native(oldWebkitcObjDoc, 'exitFullscreen')).toBeDefined();
+		});
+
+		// mozilla
+		var mozObjElem = {
+				mozRequestFullScreen: function() {}
+			},
+			mozObjDoc = {
+				mozCancelFullScreen: function() {},
+				mozFullScreenEnabled: false,
+				mozFullScreenElement: null
+			};
+
+		it('should detect -moz-prefixed non-standard FS API properties and methods', function() {
+			expect(native(mozObjElem, 'requestFullscreen')).toBeDefined();
+			expect(native(mozObjDoc, 'exitFullscreen')).toBeDefined();
+			expect(native(mozObjDoc, 'fullscreenEnabled')).toBeDefined();
+			expect(native(mozObjDoc, 'fullscreenElement')).toBeDefined();
+		});
+
+	});
+
+});
