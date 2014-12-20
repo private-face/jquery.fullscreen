136cbe7e43ac863b099e7088e7ad43a1c420a836
Vladimir Zhuravlev <private.face@gmail.com>
2014-12-20 14:28:33 +0700

    fixed "version" build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,73 @@
 module.exports = function(grunt) {
 
-  const SOURCES = [
-    'src/utils.js',
-    'src/abstract-fullscreen.js',
-    'src/native-fullscreen.js',
-    'src/fallback-fullscreen.js',
-    'src/jquery-fullscreen.js'
-  ];
+	const SOURCES = [
+		'src/utils.js',
+		'src/abstract-fullscreen.js',
+		'src/native-fullscreen.js',
+		'src/fallback-fullscreen.js',
+		'src/jquery-fullscreen.js'
+	];
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: grunt.file.read('src/build/banner.js', { encoding: 'utf8' }),
-        wrap: 'window'
-      },
-      source: {
-        options: {
-          compress: false,
-          mangle: false,
-          beautify: true
-        },
-        files: {
-          'release/<%= pkg.name.toLowerCase() %>.js': SOURCES
-        }
-      },
-      minified: {
-        options: {
-          sourceMap: true,
-          sourceMapIncludeSources: true
-        },
-        files: {
-          'release/<%= pkg.name.toLowerCase() %>.min.js': SOURCES
-        }
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['src/**/*.js', 'Gruntfile.js'],
-        tasks: ['uglify:minified'],
-        options: {
-          spawn: false,
-        },
-      },
-    }    
-  });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: grunt.file.read('src/build/banner.js', { encoding: 'utf8' }),
+				wrap: 'window'
+			},
+			source: {
+				options: {
+					compress: false,
+					mangle: false,
+					beautify: true
+				},
+				files: {
+					'release/<%= pkg.name.toLowerCase() %>.js': SOURCES
+				}
+			},
+			minified: {
+				options: {
+					sourceMap: true,
+					sourceMapIncludeSources: true
+				},
+				files: {
+					'release/<%= pkg.name.toLowerCase() %>.min.js': SOURCES
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['src/**/*.js', 'Gruntfile.js'],
+				tasks: ['uglify:minified'],
+				options: {
+					spawn: false,
+				},
+			},
+		}    
+	});
 
-  function updateJSONfile(fileName, field, value) {
-    const FILE_OPTIONS = { encoding: 'utf8' };
-    var fileContents = grunt.file.read(fileName, FILE_OPTIONS);
-    var JSON = JSON.parse(fileContents);
-    JSON[field] = value;
-    fileContents = JSON.stringify(JSON, null, '\t');
-    grunt.file.write(fileName, fileContents, FILE_OPTIONS);
-  }
+	function updateJSONfile(fileName, field, value) {
+		const FILE_OPTIONS = { encoding: 'utf8' };
+		var fileContents = grunt.file.read(fileName, FILE_OPTIONS);
+		var json = JSON.parse(fileContents);
+		json[field] = value;
+		fileContents = JSON.stringify(json, null, '\t');
+		grunt.file.write(fileName, fileContents, FILE_OPTIONS);
+	}
 
-  grunt.registerTask('version', 'update version', function(version) {
-    if (version) {
-      updateJSONfile('package.json', 'version', version);
-    } else {
-      version = grunt.config.getRaw().pkg;
-    }
-    updateJSONfile('fullscreener.jquery.json', 'version', version);
-    // updateJSONfile('bower.json', 'version', version);
-  });
+	grunt.registerTask('version', 'Update version in all project json files', function(version) {
+		if (version) {
+			updateJSONfile('package.json', 'version', version);
+		} else {
+			version = grunt.config.getRaw().pkg;
+		}
+		updateJSONfile('fullscreener.jquery.json', 'version', version);
+		// updateJSONfile('bower.json', 'version', version);
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['uglify:minified', 'watch']);
-  grunt.registerTask('release', ['uglify:source', 'uglify:minified', 'version']);
+	grunt.registerTask('default', ['uglify:minified', 'watch']);
+	grunt.registerTask('release', ['uglify:source', 'uglify:minified', 'version']);
 };
