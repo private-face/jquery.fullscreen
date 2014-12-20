b5b35390e9fe856111d93a5da5d8ca3d9ec590e1
Vladimir Zhuravlev <private.face@gmail.com>
2014-12-20 14:12:52 +0700

    switch build process to grunt

---
+++ src/build/banner.js
@@ -0,0 +1,10 @@
+/*
+ * <%= pkg.name %> v<%= pkg.version %>
+ * https://github.com/private-face/jquery.fullscreen
+ *
+ * Copyright (c) 2012–<%= grunt.template.today("yyyy") %> Vladimir Zhuravlev
+ * Released under the MIT license
+ * https://github.com/private-face/jquery.fullscreen/blob/master/LICENSE
+ *
+ * Date: <%= grunt.template.today("yyyy-mm-dd") %>
+ **/
